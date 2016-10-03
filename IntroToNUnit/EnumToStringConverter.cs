20cb76f27f3b6cf26c55ad307984eeef39da8fbc
Rob Prouse <rob@prouse.org>
2016-10-03 17:36:19 -0400

    Code for the Introduction to Unit Tests with NUnit blog post

---
+++ IntroToNUnit/EnumToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IntroToNUnit
+{
+    /// <summary>
+    /// Converts an enum to a friendly string by breaking apart words on capital letters
+    /// </summary>
+    public static class EnumToStringConverter
+    {
+        public static string ToFriendlyString(this Enum value)
+        {
+            var text = value.ToString();
+            var regex = new Regex(@"\p{Lu}\p{Ll}*");
+            var split = from Match match in regex.Matches(text) select match.Value;
+            return string.Join(" ", split);
+        }
+    }
+}
