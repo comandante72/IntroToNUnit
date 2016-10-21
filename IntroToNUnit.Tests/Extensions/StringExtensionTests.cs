94362a0e3abaf909e726f1c189ae6cf0ac1ede6d
Rob Prouse <rob@prouse.org>
2016-10-21 09:27:45 -0400

    Unit tests for the third NUnit blog post

---
+++ IntroToNUnit.Tests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using IntroToNUnit.Extensions;
+using System.Linq;
+using System;
+
+namespace IntroToNUnit.Tests.Extensions
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [Test]
+        public void StringContainsReturnsTrueIfStringIsFound()
+        {
+            string str = "The quick brown fox jumps over the lazy dog.";
+            string substr = "brown fox";
+
+            bool found = str.Contains(substr, StringComparison.CurrentCulture);
+
+            Assert.That(found, Is.True);
+        }
+
+        [Test]
+        public void StringContainsReturnsFalseIfStringIsNotFound()
+        {
+            string str = "The quick brown fox jumps over the lazy dog.";
+            string substr = "lazy fox";
+
+            bool found = str.Contains(substr, StringComparison.CurrentCulture);
+
+            Assert.That(found, Is.False);
+        }
+
+        [Test]
+        public void StringContainsCanIgnoreCase()
+        {
+            string str = "The quick brown fox jumps over the lazy dog.";
+            string substr = "the quick brown fox";
+
+            bool found = str.Contains(substr, StringComparison.CurrentCultureIgnoreCase);
+
+            Assert.That(found, Is.True);
+        }
+    }
+}
